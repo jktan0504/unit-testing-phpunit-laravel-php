f0a94b48037d6f083d23cd1bdcf185b3bc266aa8
JK TAN <jktan@JKs-MacBook-Pro.local>
2019-12-05 17:23:38 +0800

    added BeverageTest

---
+++ tests/Unit/BeverageTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BeverageTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_beverage_name_attribute()
+    {
+        // create beverage
+
+        // get beverage name 
+
+        assert
+        $this->assertTrue(true);
+    }
+}
