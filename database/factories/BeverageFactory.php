7294b7b6dd862b3d31676fa68c92a095497d0353
JK TAN <jktan@JKs-MacBook-Pro.local>
2019-12-05 17:24:42 +0800

    added Beverage Model & Factory

---
+++ database/factories/BeverageFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Beverage;
+use Faker\Generator as Faker;
+
+$factory->define(Beverage::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
